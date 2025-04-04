1e0eb48d54b8214496313e45ec572fd49d1069f0
Jihyun <144994656+dxxrjh@users.noreply.github.com>
2025-04-05 02:27:16 +0900

    EC2 서버로 변경

--- src/index.js
+++ src/index.js
@@ -113,6 +113,6 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(port, () => {
-    console.log(`✅ Server running on http://localhost:${port}`);
-});
+app.listen(port, '0.0.0.0', () => {
+    console.log(`✅ Server running on port ${port}`);
+  });
