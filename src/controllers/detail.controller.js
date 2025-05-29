9daffaabc09140c46b978675eec7246b8002590c
Jihyun <jihyun@Jihyunui-MacBookAir.local>
2025-05-29 17:48:01 +0900

    Feat: 여가 상세 페이지 리뷰 목록 조회

--- src/controllers/detail.controller.js
+++ src/controllers/detail.controller.js
@@ -1,5 +1,5 @@
 import { StatusCodes } from "http-status-codes";
-import { getActivityDetail } from "../services/detail.service.js";
+import { getActivityDetail, getActivityReview } from "../services/detail.service.js";
 
 // 상세 페이지 조회
 export const handleGetActivityDetail = async(req, res, next) => {
@@ -19,6 +19,27 @@ export const handleGetActivityDetail = async(req, res, next) => {
         res.status(StatusCodes.OK).json({ success: true, message: "여가 상세 정보가 성공적으로 조회되었습니다.", data: result });
     }catch(error){
         console.log("예약 상세 조회 오류", error.message);
+        next(error);
+    }
+}
+
+// 상세 페이지 리뷰 목록
+export const handleGetActivityReview = async(req, res, next) => {
+    try{
+        const { id } = req.query; // id: activity_id
+        const userId = req.user.id;
+
+        if(!id){
+            return res.status(400).json({message: "activity id가 필요합니다"});
+        }
+
+        const result = await getActivityReview(id);
+        if(!result){
+            throw new Error();
+        }
+        res.status(StatusCodes.OK).json({ success: true, message: "여가 리뷰 목록이 성공적으로 조회되었습니다.", data: result });
+    }catch(error){
+        console.log("여가 리뷰 조회 오류", error.message);
         next(error);
     }
 }
--- src/services/detail.service.js
+++ src/services/detail.service.js
@@ -1,4 +1,6 @@
-import { getActivityById, getActivityDetailById, convertBigIntToString } from "../repositories/detail.repository.js";
+import { getActivityById, getActivityDetailById, getActivityReviewById } from "../repositories/detail.repository.js";
+import Decimal from 'decimal.js';
+
 
 export const getActivityDetail = async(activityId, userId) => {
     const activity = await getActivityById(activityId);
@@ -9,4 +11,46 @@ export const getActivityDetail = async(activityId, userId) => {
         detail: result
       });
     
+}
+
+export const convertBigIntToString = (obj) => {
+  if (obj === null || obj === undefined) return obj;
+
+  if (typeof obj === 'bigint') {
+    return obj.toString();
+  }
+
+  if (obj instanceof Decimal) {
+    return Number(obj.toFixed(1));
+  }
+
+  // 날짜 문자열 혹은 Date 객체 처리
+  if (obj instanceof Date) {
+    return obj.toISOString().split('T')[0];  // 'YYYY-MM-DD' 형태로 변환
+  }
+  
+  // 만약 문자열인데 ISO 날짜 형식이면 그대로 리턴
+  if (typeof obj === 'string' && /^\d{4}-\d{2}-\d{2}/.test(obj)) {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map(convertBigIntToString);
+  }
+
+  if (typeof obj === 'object') {
+    const converted = {};
+    for (const key in obj) {
+      converted[key] = convertBigIntToString(obj[key]);
+    }
+    return converted;
+  }
+
+  return obj;
+};
+
+
+export const getActivityReview = async(id) => {
+  const result = await getActivityReviewById(id);
+  return await convertBigIntToString(result);
 }
